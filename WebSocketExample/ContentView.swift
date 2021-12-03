6d486c7b6a88303ff41882cda7f4db205d9a6573
ashok-khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-12-03 22:21:04 +0400

    Initial Commit

---
+++ WebSocketExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebSocketExample
+//
+//  Created by Ashok Khanna on 3/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebSocketExample/WebSocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketExampleApp.swift
+//  WebSocketExample
+//
+//  Created by Ashok Khanna on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
